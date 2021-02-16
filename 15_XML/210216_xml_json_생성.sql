14f2fe761d99c3e6ea33cd3be042625f55550caf
choiyeonseong <am3251@naver.com>
2021-02-16 15:23:53 +0900

    15장 XML

---
+++ 15_XML/210216_xml_json_생성.sql
@@ -0,0 +1,9 @@
+﻿select u.userID, u.userName, u.birthYear, u.addr, u.height, b.prodName, b.price
+	from userTBL as u
+	inner join buyTBL as b
+		on u.userID = b.userID
+	where u.height > 170
+	for json auto;	-- json 생성
+	-- for xml auto	-- xml 생성
+
+	-- 옛날에는 xml 많이 쓰고 지금은 json 많이 씀
---
+++ 15_XML/210216_xml_형식.sql
@@ -0,0 +1,7 @@
+﻿insert into xmlTBL values ('HelloWorld');
+
+select * from xmlTBL;
+
+insert into xmlTBL values ('<html><body><p>Sample HTML</p></body></html>');
+-- xml은 형식에 까다로움(html 비교적 관대)
+insert into xmlTBL values ('<html><body><p>Sample HTML</p></html></body>');
